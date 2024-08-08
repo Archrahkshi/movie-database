f9e80456e301012099eee0953f179f9baee5c2ee
Archrahkshi <firekost@gmail.com>
2024-08-09 01:41:17 +0200

    Reduce potential boilerplate

--- app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowItem.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowItem.kt
@@ -1,11 +1,11 @@
 package ru.androidschool.intensiv.ui.tvshows
 
 import android.view.View
-import com.squareup.picasso.Picasso
 import com.xwray.groupie.viewbinding.BindableItem
 import ru.androidschool.intensiv.R
 import ru.androidschool.intensiv.data.TvShow
 import ru.androidschool.intensiv.databinding.ItemTvShowBinding
+import ru.androidschool.intensiv.util.loadFromUrl
 
 class TvShowItem(
     private val content: TvShow,
@@ -15,13 +15,12 @@ class TvShowItem(
     override fun getLayout() = R.layout.item_tv_show
 
     override fun bind(viewBinding: ItemTvShowBinding, position: Int) {
-        viewBinding.tvShowName.text = content.title
-        viewBinding.tvShowRating.rating = content.voteAverage / 2
-        viewBinding.itemTvShow.setOnClickListener { onClick(content) }
-
-        Picasso.get()
-            .load("https://i.ytimg.com/vi/mkD5Nsr4vfc/maxresdefault.jpg")
-            .into(viewBinding.tvShowPreview)
+        with(viewBinding) {
+            tvShowName.text = content.title
+            tvShowRating.rating = content.voteAverage / 2
+            itemTvShow.setOnClickListener { onClick(content) }
+            tvShowPreview.loadFromUrl("https://i.ytimg.com/vi/mkD5Nsr4vfc/maxresdefault.jpg")
+        }
     }
 
     override fun initializeViewBinding(view: View) = ItemTvShowBinding.bind(view)
--- app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
@@ -4,31 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.NestedScrollView
-import androidx.fragment.app.Fragment
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
-import ru.androidschool.intensiv.R
 import ru.androidschool.intensiv.data.MockRepository
 import ru.androidschool.intensiv.data.TvShow
 import ru.androidschool.intensiv.databinding.TvShowsFragmentBinding
+import ru.androidschool.intensiv.ui.BaseFragment
 
-class TvShowsFragment : Fragment(R.layout.tv_shows_fragment) {
-    private var _binding: TvShowsFragmentBinding? = null
+class TvShowsFragment : BaseFragment<TvShowsFragmentBinding>() {
 
-    // This property is only valid between onCreateView and onDestroyView.
-    private val binding get() = _binding!!
+    override fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?) =
+        TvShowsFragmentBinding.inflate(inflater, container, false)
 
     private val adapter by lazy<GroupAdapter<GroupieViewHolder>>(::GroupAdapter)
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): NestedScrollView {
-        _binding =TvShowsFragmentBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,10 +24,5 @@ class TvShowsFragment : Fragment(R.layout.tv_shows_fragment) {
             add(TvShowItem(TvShow("A TV show with a very very very very long title", 10f)) {})
             addAll(MockRepository.getTvShows().map { TvShowItem(it) {} })
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
